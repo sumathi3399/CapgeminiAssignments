07f42d782100a08d058affc0b12d20648bb3e5a9
sumathi varma <49781370+sumathi3399@users.noreply.github.com>
2021-03-31 17:17:13 +0530

    Add files via upload

---
+++ React/src/components/Employee.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Employee() {
+    return (
+       <table>
+
+            <tr>
+                <th colSpan="2" >
+                    <td>FirstName</td>
+                    <td>LastName</td>
+                </th>
+            </tr>
+            <tr>
+                <td>Sumathi</td>
+                <td>Nadakuditi</td>
+            </tr>
+            <tr>
+                <td>Smily</td>
+                <td>Varma</td>
+            </tr>
+            <tr>
+                <td>Sri</td>
+                <td>valli</td>
+            </tr>
+            <tr>
+                <td>Hs</td>
+                <td>Tatipudi</td>
+            </tr>
+        </table>
+        
+    )
+}
+
+export default Employee
