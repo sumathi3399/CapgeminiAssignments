2e4a50c5a56dd71c82e0e26af1716c33240a80e1
sumathi varma <49781370+sumathi3399@users.noreply.github.com>
2021-03-31 17:02:24 +0530

    Delete Spring Rest directory

--- Spring Rest/5/VEmployeesRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.springbootstarter;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VEmployeesRepository extends CrudRepository<VEmployees, String> {
-  
-}
